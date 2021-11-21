a68787add6347969c5a58852a509a47bf3b3a98e
uvinduishara <uishara98@gmail.com>
2021-11-21 20:05:52 +0530

    Initial configurations.

---
+++ Fitness App/lib/util/colors.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter/material.dart';
+
+class Colors{
+  static const primaryColor =  Color(0xFF5887F6);
+  static const blackColor =  Color(0xFF222222);
+  static const grayColor =  Color(0xFF757682);
+  static const lightGrayColor =  Color(0xFFEDEDF7);
+}
